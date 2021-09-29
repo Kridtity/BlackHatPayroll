b4cc36a4217a2b7886714035d143bae44551ea91
Kridtity <46507307+Kridtity@users.noreply.github.com>
2021-09-29 19:52:47 +0800

    Updated copyright

--- BlackHat IT Payroll System.py
+++ BlackHat IT Payroll System.py
@@ -1,7 +1,7 @@
 #Author: Kridtity Ikhlaas Lawang
 #User Name: Dragon King
 #Program Name: BlackHat IT Payroll System
-#Copyright: Copyright 2021, Blackhat IT Payroll System
+#Copyright: Copyright (C) 2021 Kridtity Lawang
 
 #License: GNU GPLv3.0
 #Status: Release
