c67761d2bdf9d72654af6471e51c516550b8140a
Kridtity <46507307+Kridtity@users.noreply.github.com>
2021-09-29 20:01:52 +0800

    Updated copyright notice

--- BlackHat IT Payroll System.py
+++ BlackHat IT Payroll System.py
@@ -1,13 +1,26 @@
 #Author: Kridtity Ikhlaas Lawang
-#User Name: Dragon King
 #Program Name: BlackHat IT Payroll System
-#Copyright: Copyright (C) 2021 Kridtity Lawang
 
 #License: GNU GPLv3.0
 #Status: Release
 #Version: 1.0
 #Release Date: 29-09-2021
 #Description: Payroll calculation system for Blackhat IT
+
+#Copyright (C) 2021 Kridtity Lawang
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #Import modules (paymod is self-created)
 import paymod
